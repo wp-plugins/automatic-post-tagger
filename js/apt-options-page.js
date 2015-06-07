c722454466460d1068b015d728e663831d080bba
Devtard <Devtard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-07 23:02:30 +0000

    Tagging APT v1.8.1
    git-svn-id: https://plugins.svn.wordpress.org/automatic-post-tagger/trunk@1176395 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/apt-options-page.js
+++ js/apt-options-page.js
@@ -1,14 +1,14 @@
-function apt_toggle_widget(num){
-	var ele = document.getElementById("apt_widget_id_["+num+"]");
+function apt_set_widget_visibility(apt_widget_id){
+	var ele = document.getElementById("apt_widget_id_["+apt_widget_id+"]");
 
-	var apt_widget_id = num;
+	var apt_widget_id = apt_widget_id;
 
 	if(ele.style.display == "block"){
     	ele.style.display = "none"; //display the change immediately - we won't wait for the script, that would be too slow
 
 		//save id to db
 		var data = {
-			action: 'apt_toggle_widget',
+			action: 'apt_set_widget_visibility',
 			security: apt_options_page_nonce.security,
 			apt_widget_id: apt_widget_id,
 			};
@@ -23,7 +23,7 @@ function apt_toggle_widget(num){
 
 		//delete id from db
 		var data = {
-			action: 'apt_toggle_widget',
+			action: 'apt_set_widget_visibility',
 			security: apt_options_page_nonce.security,
 			apt_widget_id: apt_widget_id,
 			};
@@ -35,15 +35,34 @@ function apt_toggle_widget(num){
 	}
 }
 
-
 //change backgrounds of input fields
-function apt_change_background(num){
-	if (document.getElementById("apt_keywordlist_checkbox_"+num).checked){
-		document.getElementById("apt_keywordlist_keyword_"+num).style.backgroundColor='#FFD2D2';
-		document.getElementById("apt_keywordlist_related_words_"+num).style.backgroundColor='#FFD2D2';
+function apt_change_background(apt_item_editor,apt_widget_id){
+	if(apt_item_editor == 1){ //change the background in the keyword editor
+		if (document.getElementById("apt_keyword_set_list_checkbox_"+apt_widget_id).checked){
+			document.getElementById("apt_keyword_set_list_name_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+			document.getElementById("apt_keyword_set_list_related_keywords_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+			document.getElementById("apt_keyword_set_list_group_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+		}
+		else{
+			document.getElementById("apt_keyword_set_list_name_"+apt_widget_id).style.backgroundColor='';
+			document.getElementById("apt_keyword_set_list_related_keywords_"+apt_widget_id).style.backgroundColor='';
+			document.getElementById("apt_keyword_set_list_group_"+apt_widget_id).style.backgroundColor='';
+		}
 	}
-	else{
-		document.getElementById("apt_keywordlist_keyword_"+num).style.backgroundColor='';
-		document.getElementById("apt_keywordlist_related_words_"+num).style.backgroundColor='';
+	else{ //if apt_item_editor != 1, change the background in the group editor
+		if (document.getElementById("apt_configuration_group_list_checkbox_"+apt_widget_id).checked){
+			document.getElementById("apt_configuration_group_list_name_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+			document.getElementById("apt_configuration_group_list_keyword_set_count_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+			document.getElementById("apt_configuration_group_list_status_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+			document.getElementById("apt_configuration_group_list_term_limit_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+			document.getElementById("apt_configuration_group_list_taxonomies_"+apt_widget_id).style.backgroundColor='#FFD2D2';
+		}
+		else{
+			document.getElementById("apt_configuration_group_list_name_"+apt_widget_id).style.backgroundColor='';
+			document.getElementById("apt_configuration_group_list_keyword_set_count_"+apt_widget_id).style.backgroundColor='';
+			document.getElementById("apt_configuration_group_list_status_"+apt_widget_id).style.backgroundColor='';
+			document.getElementById("apt_configuration_group_list_term_limit_"+apt_widget_id).style.backgroundColor='';
+			document.getElementById("apt_configuration_group_list_taxonomies_"+apt_widget_id).style.backgroundColor='';
+		}
 	}
 }
